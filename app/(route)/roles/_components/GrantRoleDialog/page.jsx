a6f3b14875475997e7c074fc229db9bf74a3705b
AbdulSamad18090 <abdulsamad18090@gmail.com>
2024-12-16 14:35:31 +0500

    changes in ui of Grant role dialog

--- app/(route)/roles/_components/GrantRoleDialog/page.jsx
+++ app/(route)/roles/_components/GrantRoleDialog/page.jsx
@@ -19,6 +19,7 @@ const GrantRoleDialog = ({ onClose }) => {
         <h1 className="text-lg font-semibold p-2">Grant This Role To...</h1>
       </div>
       <div className="my-2">
+        <div className=" border shadow-md rounded-lg p-2 mb-3">
         <h1 className="font-bold dark:text-neutral-500">
           1. Define whome you want to grant this role permission to.
         </h1>
@@ -31,67 +32,68 @@ const GrantRoleDialog = ({ onClose }) => {
             />
           </div>
           <div className="flex flex-col">
-            <label>Grant Role To</label>
+            <label>Target Users</label>
             <select className="w-[350px] px-2 py-1 mt-1 outline focus:outline-2 outline-blue-500 rounded-lg">
               <option value="managers">Managers</option>
             </select>
-            <div className="flex items-center gap-2 my-2">
+            {/* <div className="flex items-center gap-2 my-2">
               <input type="radio" id="all-managers" />
               <label htmlFor="all-managers">All Mangers</label>
-            </div>
-            <div className="flex items-center gap-2 mb-2">
+            </div> */}
+            {/* <div className="flex items-center gap-2 mb-2">
               <input type="radio" id="only-managers" />
               <label htmlFor="only-managers">
                 Only the Mangers in these Groups below:
               </label>
-            </div>
-            <select className="w-[350px] px-2 py-1 mt-1 outline focus:outline-2 outline-blue-500 rounded-lg">
+            </div> */}
+            {/* <select className="w-[350px] px-2 py-1 mt-1 outline focus:outline-2 outline-blue-500 rounded-lg">
               <option value="group 1">Group 1</option>
               <option value="group 2">Group 2</option>
               <option value="group 3">Group 3</option>
-            </select>
-            <div className="flex items-center gap-2 my-2">
+            </select> */}
+            {/* <div className="flex items-center gap-2 my-2">
               <input type="checkbox" id="only-managers" />
               <label htmlFor="only-managers">
                 Allow their Managers to have the same permission access
               </label>
-            </div>
+            </div> */}
           </div>
         </div>
-        <div className="w-full h-[2px] bg-gray-200"></div>
+        </div>
+        {/* <div className="w-full h-[2px] bg-gray-200"></div> */}
+        <div className="border rounded-lg shadow-md p-2">
         <h1 className="font-bold dark:text-neutral-500">
-          2. Specify the target population whome the above granted users have
-          permisssion to access.
+          2.  Define group you want to grant this role permission to.
         </h1>
         <div className="flex gap-3 my-4">
           <div className="px-10">
             <Image src="/images/group-no-bg.gif" width={100} height={100} />
           </div>
           <div className="flex flex-col">
-            <label>Target Population</label>
-            <div className="flex items-center gap-2 my-2">
+            <label>Target Groups</label>
+            {/* <div className="flex items-center gap-2 my-2">
               <input type="radio" id="reports" />
               <label htmlFor="reports">Granted User's Direct Reports</label>
-            </div>
-            <div className="flex items-center gap-2 mb-2">
+            </div> */}
+            {/* <div className="flex items-center gap-2 mb-2">
               <input type="radio" id="only-reports" />
               <label htmlFor="only-reports">
                 Only the Direct Reports in these Groups below:
               </label>
-            </div>
+            </div> */}
             <select className="w-[350px] px-2 py-1 mt-1 outline focus:outline-2 outline-blue-500 rounded-lg">
               <option value="group 1">Group 1</option>
               <option value="group 2">Group 2</option>
               <option value="group 3">Group 3</option>
             </select>
-            <div className="flex items-center gap-2 my-2">
+            {/* <div className="flex items-center gap-2 my-2">
               <input type="checkbox" />
               <label>
                 Include access to the reports of the Granted User's Direct
                 Reports
               </label>
-            </div>
-            <div className="w-full flex justify-between items-center gap-10 my-2">
+            </div> */}
+            {/* <div className="w-full flex justify-between items-center gap-10 my-2">
               <div className="flex items-center gap-2">
                 <input type="checkbox" />
                 <label>Include access to the Granted User (Self)</label>
@@ -100,8 +102,9 @@ const GrantRoleDialog = ({ onClose }) => {
                 <input type="checkbox" />
                 <label>Exclude Granted Users from</label>
               </div>
-            </div>
+            </div> */}
           </div>
+        </div>
         </div>
       </div>
     </PermissionDialog>
